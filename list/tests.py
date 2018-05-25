6edcb5e49fb206eedbf996f95baa31e199f672c6
haochengz <haochengzhao@outlook.com>
2018-05-26 00:38:26 +0800

    add a few tests for index view

--- list/tests.py
+++ list/tests.py
@@ -21,3 +21,15 @@ class SomeTest(TestCase):
     def test_index_view_returns_html_page(self):
         resp = self.client.get("/")
         self.assertTemplateUsed(resp, "index.html")
+
+    def test_post_item_to_view_and_redirect_to_index_page(self):
+        resp = self.client.post("/", {})
+        self.assertEqual(resp.status_code, 302)
+        self.assertRedirects(resp, "/")
+
+    def test_post_item_to_view_and_retrieve_it(self):
+        resp = self.client.post("/", data={
+            "new_item": "do it now",
+        }, follow=True)
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, "do it now")

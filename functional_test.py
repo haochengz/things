ada2162cfc6c78be60fc067a2e65b4ab4e97b1fc
haochengz <haochengzhao@outlook.com>
2018-05-25 21:21:54 +0800

    add some functional test

--- functional_test.py
+++ functional_test.py
@@ -5,6 +5,7 @@ This Module include the TestCase of the website
 
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class FunctionalTest(unittest.TestCase):
@@ -26,11 +27,25 @@ class FunctionalTest(unittest.TestCase):
         self.browser = create_browser()
 
     def test_server_renders_index_page(self):
-        """
-        Test Server was started
-        """
         self.browser.get("http://127.0.0.1:8000")
         self.assertIn('To-do', self.browser.title)
+
+        header_text = self.browser.find_element_by_tag_name("h1").text
+        self.assertIn("To-do", header_text)
+
+        inputbox = self.browser.find_element_by_id("new_item")
+        self.assertEqual(
+            inputbox.get_attribute("placeholder"),
+            "Enter a to-do item"
+        )
+        inputbox.send_keys("Buy peacock feathers")
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id("list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
 
 def create_browser():

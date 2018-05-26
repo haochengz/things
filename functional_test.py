fc462acc38fcdbfcb0a4a63f5f2e8d7290882ccd
haochengz <haochengzhao@outlook.com>
2018-05-26 10:32:34 +0800

    add new functional test

--- functional_test.py
+++ functional_test.py
@@ -46,6 +46,16 @@ class FunctionalTest(unittest.TestCase):
         rows = table.find_elements_by_tag_name("tr")
         self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
 
+        inputbox = self.browser.find_element_by_id("new_item")
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+
+        self.browser.get('http://127.0.0.1:8000')
+        table = self.browser.find_element_by_id("list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
+        self.assertIn("2: Use peacock feathers to make a fly", [row.text for row in rows])
+
 
 def create_browser():
     """
